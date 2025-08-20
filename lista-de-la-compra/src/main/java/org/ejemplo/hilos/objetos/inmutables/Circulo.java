bf9f615dcdd1d66fc2c1b400b7cb6bb287a0535e
franklin-cruces <franklin.cruces@hotmail.com>
2025-08-20 23:38:46 +0200

    Hilos - Objetos inmutables - Clase Circulo convertida a inmutable

--- lista-de-la-compra/src/main/java/org/ejemplo/hilos/objetos/inmutables/Circulo.java
+++ lista-de-la-compra/src/main/java/org/ejemplo/hilos/objetos/inmutables/Circulo.java
@@ -2,42 +2,95 @@ package org.ejemplo.hilos.objetos.inmutables;
 
 import java.awt.*;
 
-public class Circulo {
+/*
+ * Se coloca final la clase para que no pueda ser extendida
+ * y con este y los otros cambios realizados asegurando  la inmutabilidad
+ * Otra forma seria colocar los constructores como private y reemplazarlos
+ * por metodo estaticos factoría
+ */
+//public class Circulo {
+public final class Circulo {
+    /*
+     * Las variables deben ser private y final para que
+     * no puedan ser modificadas.
+     */
+//    private double radio;
+//    private Color color;
+    private final double radio;
+    private final Color color;
 
-    private double radio;
-    private Color color;
-
+    /*
+     * El Objeto Color se crea desde afuera de la clase
+     * con cual tienen acceso total a él
+     * se debe tambien hacer una copia defensiva
+     * @param radio
+     * @param color
+     */
+//    public Circulo(double radio, Color color) {
+//        this.radio = radio;
+//        this.color = color;
+//    }
     public Circulo(double radio, Color color) {
         this.radio = radio;
-        this.color = color;
+        this.color = new Color(color.getRGB());
     }
+    /*
+     * este metodo se quita ya que incumple con la inmutabilidad ya que
+     * permite cambiar los estados del objeto (los atributos)
+     * @param radio
+     */
+//    public void establecerRadio(double radio) {
+//        this.radio = radio;
+//    }
 
-    public void establecerRadio(double radio) {
-        this.radio = radio;
-    }
+    /*
+     * este metodo se quita ya que incumple con la inmutabilidad ya que
+     * permite cambiar los estados del objeto (los atributos)
+     * @param color
+     */
+//    public void establecerColor(Color color) {
+//        this.color = color;
+//    }
 
-    public void establecerColor(Color color) {
-        this.color = color;
-    }
+    /**
 
+     * @return
+     */
     public double obtenerRadio() {
         return radio;
     }
 
+    /*
+     * se modifica para que devuelva una copia defensiva
+     * para que no tengan acceso desde afuera de la clase
+     * al objeto Color y no pueda ser modificado
+     * @return
+     */
+//    public Color obtenerColor() {
+//        return color;
+//    }
     public Color obtenerColor() {
-        return color;
+        return new Color(color.getRGB());
     }
-
     public double obtenerDiametro() {
         return radio * 2;
     }
 
     public double obtenerArea() {
-        return Math.PI  * Math.pow(radio, 2);
+        return Math.PI * Math.pow(radio, 2);
     }
 
-    public void escalar(double factor) {
-        radio *= factor;
+    /*
+     * Modificamos este metodo para que cumpla con la
+     * inmutabilidad, devolviendo un nuevo objeto escalado
+     * en lugar de modificar el objeto actual
+     * @param factor
+     */
+//    public void escalar(double factor) {
+//        radio *= factor;
+//    }
+    public Circulo escalar(double factor) {
+        return new Circulo(radio * factor, color);
     }
 
 }

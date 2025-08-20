b3a71b53533d8acd727722814c96e6e7da699d63
franklin-cruces <franklin.cruces@hotmail.com>
2025-08-20 21:57:50 +0200

    Hilos - metodos sincronizados - Synchronized

--- lista-de-la-compra/src/main/java/org/ejemplo/hilos/metodos/sincronizados/Contador.java
+++ lista-de-la-compra/src/main/java/org/ejemplo/hilos/metodos/sincronizados/Contador.java
@@ -3,15 +3,24 @@ package org.ejemplo.hilos.metodos.sincronizados;
 public class Contador {
     private int c = 0;
 
-    public void incrementar() {
+    public synchronized void incrementar() {
         c++;
+        for(int i = 0 ; i<5 ;i++){
+            System.out.println("Esperando un segundo desde el hilo " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
-    public void decrementar() {
+    public synchronized void decrementar() {
         c--;
     }
 
     public int obtenerValor() {
+        System.out.println("ObtenerValor Hilo " + Thread.currentThread().getName());
         return c;
     }
 }
--- lista-de-la-compra/src/main/java/org/ejemplo/hilos/metodos/sincronizados/Main.java
+++ lista-de-la-compra/src/main/java/org/ejemplo/hilos/metodos/sincronizados/Main.java
@@ -2,6 +2,28 @@ package org.ejemplo.hilos.metodos.sincronizados;
 
 public class Main {
     public static void main(String[] args){
+        Contador contador = new Contador();
 
+        Thread miHilo =  new Thread(new Runnable() {
+            @Override
+            public void run() {
+                contador.incrementar();
+                System.out.println("El valor del contador es " + contador.obtenerValor());
+            }
+        }, "Hilo Uno");
+
+        Thread miHiloDos =  new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //contador.incrementar();
+                //contador.decrementar();
+                System.out.println("El valor del contador es " + contador.obtenerValor());
+            }
+        }, "Hilo Dos");
+
+        miHilo.start();
+        miHiloDos.start();
     }
+
+
 }

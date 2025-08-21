e084d1d480551c40fb3007b53bbc8ca4ef0a9d8c
franklin-cruces <franklin.cruces@hotmail.com>
2025-08-21 11:32:44 +0200

    Hilos - Interfaz Lock - Se agrega Trylock

--- lista-de-la-compra/src/main/java/org/ejemplo/hilos/interfaz/lock/Impresora.java
+++ lista-de-la-compra/src/main/java/org/ejemplo/hilos/interfaz/lock/Impresora.java
@@ -5,20 +5,34 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Impresora {
 
-    private final Lock impresoraLock = new ReentrantLock();
+    private final Lock impresoraCalidadLock = new ReentrantLock();
+    private final Lock impresoraRapidaLock = new ReentrantLock();
 
     public void imprimir(Object o) {
 
-        impresoraLock.lock();
-        try {
-            double duracion = Math.random() * 10000;
-            System.out.println(Thread.currentThread().getName() + ": org.ejemplo.hilos.interfaz.lock.Impresora imprimiendo un documento durante " + duracion / 1000 + "segundos");
-            Thread.sleep((long) duracion);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println(Thread.currentThread().getName() + ": La impresión ha finalizado");
-            impresoraLock.unlock();
+        if (impresoraCalidadLock.tryLock()) {
+            try {
+                double duracion = Math.random() * 10000;
+                System.out.println(Thread.currentThread().getName() + ": Impresora de calidad imprimiendo un documento durante " + duracion / 1000 + "segundos");
+                Thread.sleep((long) duracion);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(Thread.currentThread().getName() + ": La impresión ha finalizado");
+                impresoraCalidadLock.unlock();
+            }
+        }else {
+            impresoraRapidaLock.lock();
+            try {
+                double duracion = Math.random() * 1000;
+                System.out.println(Thread.currentThread().getName() + ": Impresora rapida imprimiendo un documento durante " + duracion / 1000 + "segundos");
+                Thread.sleep((long) duracion);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(Thread.currentThread().getName() + ": La impresión ha finalizado");
+                impresoraRapidaLock.unlock();
+            }
         }
     }
 }
